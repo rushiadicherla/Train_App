442d84fb80b64ad3d2a0c1ba0712daad876648dd
Rushi <rushiadicherla@gmail.com>
2025-06-05 22:35:53 +0530

    Update user.h

--- user.h
+++ user.h
@@ -3,11 +3,13 @@
 
 #define MAX_NAME 50
 #define MAX_PASS 20
+
 typedef struct User {
     char username[MAX_NAME];
     char password[MAX_PASS];
     struct User *next;
 }User;
+
 User * load_users();
 void save_user(User *);
 int sign_up();
