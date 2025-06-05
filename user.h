4da73abf6d7834491184f0a5441a98695efe2f62
rushiadicherla <rushiadicherla@gmail.com>
2025-06-05 21:50:30 +0530

    Update user.h

--- user.h
+++ user.h
@@ -1,6 +1,17 @@
 #ifndef USER_H
 #define USER_H
-int signup();
-int signin();
-int validate_password(char *pass);
+
+#define MAX_NAME 50
+#define MAX_PASS 20
+typedef struct User {
+    char username[MAX_NAME];
+    char password[MAX_PASS];
+    struct User *next;
+}User;
+User * load_users();
+void save_user(User *);
+int sign_up();
+int sign_in();
+int is_valid_password(const char *);
+
 #endif
